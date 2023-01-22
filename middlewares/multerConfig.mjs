8b5500fb7e729be98b7d721b02f5d92448cdc030
obaidmuneer <obaidmuneer55@gmail.com>
2023-01-22 17:32:38 +0500

    .

--- middlewares/multerConfig.mjs
+++ middlewares/multerConfig.mjs
@@ -1,24 +1,14 @@
 import multer from 'multer'
 
 const storage = multer.diskStorage({
-    destination: './uploads/',
     filename: function (req, file, cb) {
         const uniqeSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
         cb(null, uniqeSuffix + '-' + file.originalname)
     }
 })
 
-// const fileFilter = (req, file, cb) => {
-//     if (!file.mimetype.match(/pdf|txt$i/)) {
-//         cb(new Error('Bad file type', false))
-//     }
-//     cb(null, true)
-// }
-
 const upload = multer({
     storage: storage,
-    // limits: { fileSize: 1024 * 1024 * 3 },
-    // fileFilter: fileFilter,
 })
 
 export default upload
