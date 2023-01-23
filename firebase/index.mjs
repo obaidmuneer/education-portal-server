940587f4900b25ccbaf59c40e5792df4bf16be31
obaidmuneer <obaidmuneer55@gmail.com>
2023-01-23 23:39:45 +0500

    .

--- firebase/index.mjs
+++ firebase/index.mjs
@@ -1,10 +1,14 @@
 import admin from 'firebase-admin'
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 var serviceAccount = {
     "type": process.env.TYPE,
     "project_id": process.env.PROJECT_ID,
     "private_key_id": process.env.PRIVATE_KEY_ID,
-    "private_key": process.env.PRIVATE_KEY,
+    "private_key": process.env.PRIVATE_KEY
+        ? process.env.PRIVATE_KEY.replace(/\\n/g, '\n')
+        : undefined,
     "client_email": process.env.CLIENT_EMAIL,
     "client_id": process.env.CLIENT_ID,
     "auth_uri": process.env.AUTH_URI,
